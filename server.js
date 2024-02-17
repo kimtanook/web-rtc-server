2be3b03509833606c4f3f1f29a0b683342522aa7
Dever-Ian <seob6402@gmail.com>
2024-02-17 23:39:35 +0900

    create: web-rtc-server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const {createServer} = require("http");
+
+const app = express();
+app.use(cors);
+const http = createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connect!!");
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
+
+http.listen(4000, () => {
+  console.log("서버 열림");
+});
