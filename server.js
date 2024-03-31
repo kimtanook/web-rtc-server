9840819c2410ead4539dfb019066aa1f6b9080e9
kimtanook <seob6402@gmail.com>
2024-03-31 20:46:52 +0900

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const {createServer} = require("http");
 
+const port = 80;
+
 const app = express();
 app.use(cors);
 const http = createServer(app);
@@ -14,8 +16,20 @@ const io = require("socket.io")(http, {
 
 io.on("connection", (socket) => {
   console.log("Connect!!");
-  socket.on("join_room", (roomName) => {
-    socket.join(roomName);
+  socket.on("join_room", ({roomName, userName}) => {
+    socket.join(roomName, () => {
+      // 소켓에 사용자 이름 저장
+      socket.userName = userName;
+
+      // 방에 조인 후 해당 방의 소켓 목록과 사용자 이름 확인
+      const roomSockets = io.sockets.adapter.rooms[roomName].sockets;
+      const socketNames = Object.keys(roomSockets).map((socketId) => {
+        const userSocket = io.sockets.sockets[socketId];
+        console.log(`Room: ${roomName}, Users: ${socketNames.join(", ")}`);
+        return `${userSocket.userName} (${socketId})`;
+      });
+    });
+    console.log("roomName : ", roomName);
     socket.to(roomName).emit("welcome");
   });
   socket.on("offer", (offer, roomName) => {
@@ -29,6 +43,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(4000, () => {
-  console.log("서버 열림");
+http.listen(port, () => {
+  console.log(port + "서버 열림");
 });
